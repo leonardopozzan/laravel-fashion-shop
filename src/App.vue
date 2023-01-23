53e7f71b725de73452cd7e6fccdf4000dac6857e
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2023-01-23 11:27:05 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <HeaderComponent/>
+  <div>
+     {{ title }}
+  </div>
+  <FooterComponent/>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+
+
+  export default {
+    data() {
+        return {
+            title: "Hello world!"
+        };
+    },
+    components: { HeaderComponent , FooterComponent }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
